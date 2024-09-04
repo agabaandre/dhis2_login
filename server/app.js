e8f2a9d10b7fd0160f013a8b4978c6ba06a2633f
Agaba Andrew <agabaandre@gmail.com>
2024-09-05 02:27:27 +0300

    fix

--- server/app.js
+++ server/app.js
@@ -3,35 +3,12 @@ const express = require('express');
 const axios = require('axios');
 const qs = require('querystring');
 const cors = require('cors');
-const morgan = require('morgan');
+const morgan = require('morgan'); // Import morgan
 
 const app = express();
 const port = 3000;
 
-// Create a variable to store cookies
-let cookies = null;
-
-// Setup axios interceptors
-axios.interceptors.request.use(config => {
-    if (cookies) {
-        config.headers.Cookie = cookies;
-    }
-    return config;
-});
-
-axios.interceptors.response.use(response => {
-    const setCookieHeader = response.headers['set-cookie'];
-    if (setCookieHeader) {
-        cookies = setCookieHeader.join(';');
-    }
-    return response;
-});
-
-app.use(cors({
-    origin: process.env.FRONTEND_URL, // Ensure this matches exactly the client's origin
-    credentials: true // Essential for cookies to be accepted on cross-origin requests
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev')); // Log requests to the console in 'dev' format
 
@@ -48,17 +25,17 @@ app.post('/node_app/login', async (req, res) => {
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
             withCredentials: true,
-            maxRedirects: 0, // Do not follow redirects
+            maxRedirects: 2, // Do not follow redirects
             validateStatus: status => status >= 200 && status < 400 // Accept all 2xx and 3xx statuses
         });
 
         console.log('Login response:', loginResponse.status, loginResponse.headers);
 
-        if (cookies) {
-            res.setHeader('Set-Cookie', cookies); // Forward cookies to the client
+        if (loginResponse.headers['set-cookie']) {
+            const dashboardUrl = process.env.DHIS2_DASHBOARD_URL;
             res.json({
                 message: 'Login successful',
-                dashboardUrl: process.env.DHIS2_DASHBOARD_URL
+                dashboardUrl: dashboardUrl
             });
         } else {
             res.status(401).json({ message: 'Login failed' });
@@ -68,6 +45,7 @@ app.post('/node_app/login', async (req, res) => {
         res.status(500).json({ message: 'Server error', error: error.message });
     }
 });
+
 
 // Start the server
 app.listen(port, () => {
